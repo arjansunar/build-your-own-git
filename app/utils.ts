e5ea19256d00a74a9863a4fef70f4bb97762ec8f
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-08-06 10:12:08 +0545

    feat: add \n character to the end

--- app/utils.ts
+++ app/utils.ts
@@ -2,5 +2,5 @@ export function parseTreeContentAndGetNames(treeContent: string) {
 
   const treeContentBlock = treeContent.split('\0').slice(1, -1).join('\0').trim()
   const treeContentBlocks = treeContentBlock?.split('\0').flatMap(block => block.split(' ').at(-1))
-  return treeContentBlocks?.join('\n')
+  return treeContentBlocks?.join('\n').concat('\n')
 }
