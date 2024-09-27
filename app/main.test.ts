85632fb11672f8c2900e0dadefa967a17921e1f0
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-09-27 21:24:36 +0545

    feat: remove unused test

--- app/main.test.ts
+++ app/main.test.ts
@@ -1,9 +1,4 @@
-import {
-  commitTree,
-  commitTreeString,
-  parseTreeContentAndGetNames,
-  writeTree,
-} from "./utils";
+import { parseTreeContentAndGetNames } from "./utils";
 import { expect, test } from "bun:test";
 
 test("Parse out tree content", () => {
@@ -11,14 +6,4 @@ test("Parse out tree content", () => {
     `tree 113\0 140000 test.1\0shasomething100644 test.3\0shasomething`,
   );
   expect(output).toBe("test.1\ntest.3\n");
-});
-
-test("Test commit tree command", () => {
-  const hash = commitTree(
-    "9c339ece773f838a4862c8e79b5e921289e86828",
-    "monkey humpty yikes donkey humpty scooby",
-    "c58187b103ae3942fac796f0a2cc473b7e9841b2",
-  );
-
-  expect(hash).toBe("034421c21c4b1806af879db89d5047c4c3f93b6a");
 });
